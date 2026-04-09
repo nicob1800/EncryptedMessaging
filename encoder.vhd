732042e3dc4c471f7683db3ef7875f99db001605
Nicolas Benavides <76823475+nicob1800@users.noreply.github.com>
2026-04-08 20:05:28 -0400

    translated encoder logic from verilog to VHDL

--- encoder.vhd
+++ encoder.vhd
@@ -1,1 +1,14 @@
 ENTITY encoder IS
+    PORT (
+        cipher : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        message : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        cipher_text : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+    );
+END encoder;
+
+ARCHITECTURE encoder_arch OF encoder IS
+BEGIN
+
+    cipher_text <= cipher(7 DOWNTO 0) XOR message;
+
+END encoder_arch;
