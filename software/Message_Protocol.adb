c3eaed673a9922f447b4a59ec5af50baa78f05fd
Nicolas Benavides <76823475+nicob1800@users.noreply.github.com>
2026-04-09 16:48:16 -0400

    Enhance message protocol procedures to include status handling for Write_Message, Read_Header, and Read_Payload functions

--- software/Message_Protocol.adb
+++ software/Message_Protocol.adb
@@ -6,8 +6,18 @@ package body Message_Protocol is
     procedure Write_Message
        (Message_Header  : Header;
         Message_Payload : byte_array;
-        Channel         : Stream_Access) is
+        Channel         : Stream_Access;
+        Status          : out Protocol_Status)
+    is
+        Header_Length : constant Integer :=
+           Integer (Message_Header.Message_Length);
     begin
+        if Header_Length <= 0 or else Message_Payload'Length /= Header_Length
+        then
+            Status := Bad_Length;
+            return;
+        end if;
+
         Unsigned_16'Write (Channel, Message_Header.Sender_ID);
         Unsigned_16'Write (Channel, Message_Header.Receiver_ID);
         Unsigned_16'Write (Channel, Message_Header.Message_Length);
@@ -17,10 +27,13 @@ package body Message_Protocol is
             Unsigned_8'Write (Channel, char);
         end loop;
 
+        Status := Ok;
     end Write_Message;
 
     procedure Read_Header
-       (Message_Header : out Header; Channel : Stream_Access) is
+       (Message_Header : out Header;
+        Channel        : Stream_Access;
+        Status         : out Protocol_Status) is
     begin
         Message_Header.Sender_ID := Unsigned_16'Input (Channel);
         Message_Header.Receiver_ID := Unsigned_16'Input (Channel);
@@ -28,24 +41,37 @@ package body Message_Protocol is
         Message_Header.Seed := Unsigned_16'Input (Channel);
         Message_Header.Counter := Unsigned_16'Input (Channel);
 
+        Status := Ok;
+    exception
+        when others =>
+            Status := Stream_Error;
     end Read_Header;
 
     procedure Read_Payload
        (Message_Payload : out byte_array;
         Payload_Length  : Unsigned_16;
-        Channel         : Stream_Access)
+        Channel         : Stream_Access;
+        Status          : out Protocol_Status)
     is
         Length_Integer : Integer := Integer (Payload_Length);
     begin
-        if (Message_Payload'Length = Length_Integer) and Length_Integer /= 0
+        if Length_Integer <= 0 or else Message_Payload'Length /= Length_Integer
         then
-            for I in 1 .. Length_Integer loop
-                Message_Payload (I) := Unsigned_8'Input (Channel);
-
-            end loop;
-        else
-            Put ("***ERROR: Payload length value does not match!***");
+            Status := Bad_Length;
+            return;
         end if;
+
+        for I in 1 .. Length_Integer loop
+            begin
+                Message_Payload (I) := Unsigned_8'Input (Channel);
+            exception
+                when others =>
+                    Status := Stream_Error;
+                    return;
+            end;
+        end loop;
+
+        Status := Ok;
 
     end Read_Payload;
 
--- software/Message_Protocol.ads
+++ software/Message_Protocol.ads
@@ -1,8 +1,10 @@
-with Interfaces;      use Interfaces;
-with GNAT.Sockets;    use GNAT.Sockets;
+with Interfaces;        use Interfaces;
+with GNAT.Sockets;      use GNAT.Sockets;
 with String_Conversion; use String_Conversion;
 
 package Message_Protocol is
+   type Protocol_Status is (Ok, Bad_Length, Stream_Error);
+
    type Header is record
       Sender_ID      : Unsigned_16;
       Receiver_ID    : Unsigned_16;
@@ -14,14 +16,18 @@ package Message_Protocol is
    procedure Write_Message
      (Message_Header  : Header;
       Message_Payload : byte_array;
-      Channel         : Stream_Access);
+      Channel         : Stream_Access;
+      Status          : out Protocol_Status);
 
    procedure Read_Header
-     (Message_Header : out Header; Channel : Stream_Access);
+     (Message_Header : out Header;
+      Channel        : Stream_Access;
+      Status         : out Protocol_Status);
 
    procedure Read_Payload
      (Message_Payload : out byte_array;
       Payload_Length  : Unsigned_16;
-      Channel         : Stream_Access);
+      Channel         : Stream_Access;
+      Status          : out Protocol_Status);
 
 end Message_Protocol;
