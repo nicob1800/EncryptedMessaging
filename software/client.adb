4bd4c78c7beaab0f21adfbfd954598f277768dcb
Nicolas Benavides <76823475+nicob1800@users.noreply.github.com>
2026-04-09 13:04:24 -0400

    Refactor client procedure to enhance message handling; replace hardcoded message with user input and streamline data sending and receiving logic.

--- software/client.adb
+++ software/client.adb
@@ -4,13 +4,12 @@ with String_Conversion; use String_Conversion;
 with Interfaces;        use Interfaces;
 
 procedure client is
-   Message       : String := "message";
-   Server_IP     : constant String :=
-     "127.0.0.1"; -- hard coded server ip address
+   Server_IP     : constant String := "127.0.0.1";
    Client_Socket : Socket_Type;
    Address       : Sock_Addr_Type;
    Channel       : Stream_Access;
-
+   Input_Buffer  : String (1 .. 200);
+   Last          : Natural;
 begin
    Put ("Connecting to " & Server_IP & "...");
    Create_Socket (Client_Socket);
@@ -20,29 +19,45 @@ begin
    Put_Line ("Connected.");
 
    Channel := Stream (Client_Socket);
-   declare
-      bitwise_message : String_Conversion.byte_array :=
-        String_To_Bytes (message);
-   begin
-      Put ("Sending data...");
-      byte_array'Output (Channel, bitwise_message);
-      Put_Line ("Sent.");
+   loop
+      Put ("Enter message (quit to stop): ");
+      Get_Line (Input_Buffer, Last);
 
-      Put ("Receiving data...");
-   end;
+      if Last = 0 or else Input_Buffer (1 .. Last) = "quit" then
+         exit;
+      end if;
 
-   declare
-      Received_Data   : byte_array := byte_array'Input (Channel);
-      Received_String : String (1 .. Received_Data'Length);
-   begin
-      for I in Received_Data'Range loop
-         Received_String (I) := Character'Val (Integer (Received_Data (I)));
-      end loop;
+      declare
+         Message_To_Send : constant String := Input_Buffer (1 .. Last);
+         Bitwise_Message : byte_array := String_To_Bytes (Message_To_Send);
+         Send_Length     : constant Natural := Bitwise_Message'Length;
+         Received_Length : Natural;
+      begin
+         Put ("Sending data...");
+         Natural'Write (Channel, Send_Length);
+         for I in Bitwise_Message'Range loop
+            Unsigned_8'Write (Channel, Bitwise_Message (I));
+         end loop;
+         Put_Line ("Sent.");
 
-      Put_Line ("Received.");
-      Put_Line (Received_String);
-   end;
+         Put ("Receiving data...");
+         Natural'Read (Channel, Received_Length);
+
+         declare
+            Received_Data   : byte_array (1 .. Positive (Received_Length));
+            Received_String : String (1 .. Integer (Received_Length));
+         begin
+            for I in Received_Data'Range loop
+               Unsigned_8'Read (Channel, Received_Data (I));
+               Received_String (I) :=
+                 Character'Val (Integer (Received_Data (I)));
+            end loop;
+
+            Put_Line ("Received.");
+            Put_Line (Received_String);
+         end;
+      end;
+   end loop;
 
    Close_Socket (Client_Socket);
-
 end client;
