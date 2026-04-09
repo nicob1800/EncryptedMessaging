6bc5e09b4347789b0e9657f757a2cc8e9083a373
Nicolas Benavides <76823475+nicob1800@users.noreply.github.com>
2026-04-08 20:35:07 -0400

    add encrypt_top entity and architecture for message encryption

---
+++ encrypt_top.vhd
@@ -0,0 +1,31 @@
+ENTITY encrypt_top IS
+    PORT (
+        message : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        treset_n : IN STD_LOGIC;
+        tclk : IN STD_LOGIC;
+        encrypted : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+    );
+END encrypt_top;
+
+ARCHITECTURE encrypt_top_arch OF encrypt_top IS
+
+    SIGNAL cipher_wire : STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL encrypted_message_wire : STD_LOGIC_VECTOR(7 DOWNTO 0);
+
+BEGIN
+    encrypted <= encrypted_message_wire;
+
+    u_LFSR : ENTITY work.LFSR(encrypt_top_arch)
+        PORT MAP(
+            clk => tclk,
+            reset_n => treset_n,
+            cipher_out => cipher_wire
+        );
+    u_encoder : ENTITY work.encoder(encrypt_top_arch)
+        PORT MAP(
+            cipher => cipher_wire,
+            message => message,
+            cipher_text => encrypted_message_wire
+        );
+END
+END encrypt_top_arch;
